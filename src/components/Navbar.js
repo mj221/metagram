78eae47398aadb3137548ca7bcf15330badc336a
MJ LEE <mj1201276@yahoo.com.au>
2022-02-16 02:00:36 +1000

    Added improved loading screen and wip theme toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="."
@@ -18,7 +18,7 @@ class Navbar extends Component {
           Meta Gram
         </a>
         <ul className="navbar-nav px-3">
-        <li className="navbar-item text-nowrap d-none d-sm-none d-sm-block">
+            <li className="navbar-item text-nowrap d-none d-sm-none d-sm-block">
                 {this.props.account 
                     ? <><img
                   className="mr-2"
